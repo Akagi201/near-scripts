b123e7daff79ed52374b4cd5d230b7f67fca0fd4
Akagi201 <akagi201@gmail.com>
2023-02-15 15:09:40 +0800

    feat: display all keys

--- src/accounts/generate_seed_phrase.ts
+++ src/accounts/generate_seed_phrase.ts
@@ -1,5 +1,7 @@
 const { parseSeedPhrase, generateSeedPhrase } = require("near-seed-phrase");
 
-const { seedPhrase } = generateSeedPhrase();
+const { seedPhrase, publicKey, secretKey } = generateSeedPhrase();
 
-console.log(seedPhrase);
+console.log("seed phrase: %s", seedPhrase);
+console.log("public key: %s", publicKey);
+console.log("secret key: %s", secretKey);
